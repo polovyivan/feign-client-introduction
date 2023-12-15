86529a8af578b9d73aa925ccb91eb1240f2cbb0a
Ivan Polovyi <ivan.polovyi@alianza.com>
2023-12-15 20:14:29 -0300

    second commit

--- spring-customer-app/src/main/java/com/polovyi/ivan/dto/PartiallyUpdateCustomerRequest.java
+++ spring-customer-app/src/main/java/com/polovyi/ivan/dto/PartiallyUpdateCustomerRequest.java
@@ -2,12 +2,10 @@ package com.polovyi.ivan.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PartiallyUpdateCustomerRequest {
